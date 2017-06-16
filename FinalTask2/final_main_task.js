1a06e09d54db7fa8816e80230c722be80198f974
DESKTOP-84ADJI6\watar <wataru.sakata@kojimalab.com>
2017-06-16 16:58:50 +0900

    finalTask2 add

---
+++ FinalTask2/final_main_task.js
@@ -0,0 +1,107 @@
+function main()
+{
+    var volume = new KVS.LobsterData();
+    var screen = new KVS.THREEScreen();
+
+    var reflection = true;
+    var shading = true;
+
+    var isovalue = 128;
+
+    var slider = document.querySelector(".slider"), 
+        display = new Display(".value", 128);
+
+    slider.addEventListener("input", function() {
+        // ドラッグ中のイベント
+        display.setValue(this.value);
+    }, false);
+
+    slider.addEventListener("change", function() {
+        // マウスアップした際のイベント
+        isovalue = this.value;
+        display.setValue(this.value);
+        screen.scene.remove( surfaces );
+        surfaces = Isosurfaces( volume, isovalue, screen.light, screen.camera, reflection, shading );
+        screen.scene.add( surfaces );
+    }, false);
+
+    var lamb_button = document.getElementById("lamb_ref");
+    var phong_button = document.getElementById("phong_ref");
+
+    lamb_button.addEventListener("click", function() {
+        reflection = true;
+        screen.scene.remove( surfaces );
+        surfaces = Isosurfaces( volume, isovalue, screen.light, screen.camera, reflection, shading );
+        screen.scene.add( surfaces );
+    }, false);
+    phong_button.addEventListener("click", function() {
+        reflection = false;
+        screen.scene.remove( surfaces );
+        surfaces = Isosurfaces( volume, isovalue, screen.light, screen.camera, reflection, shading );
+        screen.scene.add( surfaces );
+    }, false);
+
+    var gouraud_button = document.getElementById("gouraud_shad");
+    var phong_shad_button = document.getElementById("phong_shad");
+
+    gouraud_button.addEventListener("click", function() {
+        shading = true;
+        screen.scene.remove( surfaces );
+        surfaces = Isosurfaces( volume, isovalue, screen.light, screen.camera, reflection, shading );
+        screen.scene.add( surfaces );
+    }, false);
+    phong_shad_button.addEventListener("click", function() {
+        shading = false;
+        screen.scene.remove( surfaces );
+        surfaces = Isosurfaces( volume, isovalue, screen.light, screen.camera, reflection, shading );
+        screen.scene.add( surfaces );
+    }, false);
+
+    screen.init( volume, {
+        width: window.innerWidth * 0.8,
+        height: window.innerHeight,
+        enableAutoResize: false
+    });
+
+    var bounds = Bounds( volume );
+    screen.scene.add( bounds );
+
+    var surfaces = Isosurfaces( volume, isovalue, screen.light, screen.camera, reflection, shading );
+    screen.scene.add( surfaces );
+
+    document.addEventListener( 'mousemove', function() {
+        screen.light.position.copy( screen.camera.position );
+    });
+
+    window.addEventListener( 'resize', function() {
+        screen.resize( [ window.innerWidth * 0.8, window.innerHeight ] );
+    });
+
+    screen.loop();
+
+
+    function Display(selector, initVal) {
+        var elm = document.querySelector(selector);
+
+        _init(initVal);
+
+        function _init(initVal) {
+            setValue(initVal);
+        }
+
+        function setValue(value) {
+            elm.innerText = value - 0;
+        }
+
+        function getValue() {
+            return elm.innerText - 0;
+        }
+
+        return{
+            setValue: setValue,
+            getValue: getValue,
+        };
+    }
+
+
+}
